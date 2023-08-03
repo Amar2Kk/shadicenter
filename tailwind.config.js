6cb1a8877c5c063e5042b81fbf8946cd35b63916
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2023-08-03 17:12:56 +0300

    install daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#fff",
-        secondary: "#33B14D",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#a991f7",
+                    secondary: "#f6d860",
+                    accent: "#37cdbe",
+                    neutral: "#3d4451",
+                    "base-100": "#ffffff",
+                },
+            },
+            "dark",
+            "cupcake",
+        ],
     },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
+    theme: {
+        extend: {
+            colors: {
+                primary: "#fff",
+                secondary: "#33B14D",
+                dimWhite: "rgba(255, 255, 255, 0.7)",
+                dimBlue: "rgba(9, 151, 124, 0.1)",
+            },
+        },
+        screens: {
+            xs: "480px",
+            ss: "620px",
+            sm: "768px",
+            md: "1060px",
+            lg: "1200px",
+            xl: "1700px",
+        },
     },
-  },
-  plugins: [
-  ],
-}
+    plugins: [require("daisyui")],
+};
