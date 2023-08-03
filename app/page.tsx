91eaa96245d205c9bcf84b08272b95a40678e08c
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2023-08-03 17:14:01 +0300

    enhance the structure of components folder and recreate navbar component with daisy ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,15 @@
 import styles from "./styles";
 
 import {
-  Navbar,
-  Hero,
-  AboutDr,
-  AboutUs,
-  Services,
-  Products,
-  Contact,
-  Footer,
-} from "./components";
+    Hero,
+    AboutDr,
+    AboutUs,
+    Services,
+    Products,
+    Contact,
+    Footer,
+} from "../components";
 
 export default function Home() {
-  return (
-    <main>
-      {/* Main Container */}
-      <div className="bg-primary w-full overflow-hidden">
-        {/* Navbar wrapper */}
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Navbar />
-          </div>
-        </div>
-
-        {/* Hero wrapper */}
-        <div className={` bg-primary ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Hero />
-          </div>
-        </div>
-
-        {/* sections wrapper */}
-        <div className={` bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
-            <AboutDr />
-            <Products />
-            <Services />
-            <AboutUs />
-            <Contact />
-            <Footer />
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+    return <div></div>;
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a>الرئيسية</a>
+                        </li>
+                        <li>
+                            <a>د / علاء</a>
+                        </li>
+                        <li>
+                            <a>منتجاتنا</a>
+                        </li>
+                        <li>
+                            <a href="#">خدماتنا</a>
+                        </li>
+                        <li>
+                            <a href="#">مركز الصباح</a>
+                        </li>
+                        <li>
+                            <a href="#">تواصل معنا</a>
+                        </li>
+                    </ul>
+                </div>
+                <Image src="/logo.png" width={64} height={64} alt="logo" />
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <a>الرئيسية</a>
+                    </li>
+                    <li>
+                        <a>د / علاء</a>
+                    </li>
+                    <li>
+                        <a>منتجاتنا</a>
+                    </li>
+                    <li>
+                        <a href="#">خدماتنا</a>
+                    </li>
+                    <li>
+                        <a href="#">مركز الصباح</a>
+                    </li>
+                    <li>
+                        <a href="#">تواصل معنا</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">تواصل معنا</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
